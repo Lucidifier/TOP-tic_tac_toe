0af9fb01b1998ec47c30a1aae13ab7f7ee1ee260
Lucidifier <dragan.hardi3@gmail.com>
2024-02-26 01:28:44 +0100

    add  basic page layout and styling

--- scripts.js
+++ scripts.js
@@ -23,13 +23,7 @@ const players =  (function () {
     return {player1,  player2};
 })();
 
-function controlGameFlow() {
-    function checkForTie() {
-        if (gameBoard.turnCounter = 11) {
-            console.log('TIE');
-        };
-    }
-
+const gameFlow = (function () {
     function checkWinConditions() {
         const r1 = gameBoard.boardGrid[0][0] + gameBoard.boardGrid[0][1] + gameBoard.boardGrid[0][2];
         const r2 = gameBoard.boardGrid[1][0] + gameBoard.boardGrid[1][1] + gameBoard.boardGrid[1][2];
@@ -39,73 +33,68 @@ function controlGameFlow() {
         const c3 = gameBoard.boardGrid[0][2] + gameBoard.boardGrid[1][2] + gameBoard.boardGrid[2][2];
         const d1 = gameBoard.boardGrid[0][0] + gameBoard.boardGrid[1][1] + gameBoard.boardGrid[2][2];
         const d2 = gameBoard.boardGrid[2][0] + gameBoard.boardGrid[1][1] + gameBoard.boardGrid[0][2];
-    
         if ((r1 === 0) || (r1 === 3)) {
             if(r1 === 0) {
-                return console.log('player1 wins');
+                 console.log('player1 wins');
             } else if (r1 === 3) {
-               return console.log('player2 wins')
+                console.log('player2 wins')
             };
-        } else if ((r2 === 0) || (r1 === 3)) {
+        } else if ((r2 === 0) || (r2 === 3)) {
                 if(r2 === 0) {
-                    return console.log('player1 wins');
+                     console.log('player1 wins');
                 } else if (r2 === 3) {
-                    return console.log('player2 wins')
+                     console.log('player2 wins')
                 };
         } else if ((r3 === 0) || (r3 === 3)) {
                 if(r3 === 0) {
-                    return console.log('player1 wins');
+                     console.log('player1 wins');
                 } else if (r3 === 3) {
-                    return console.log('player2 wins')
+                     console.log('player2 wins')
                 };
-        } else if ((c1 === 0) || (r1 === 3)) {
+        } else if ((c1 === 0) || (c1 === 3)) {
             if(c1 === 0) {
-                return console.log('player1 wins');
+                 console.log('player1 wins');
             } else if (c1 === 3) {
-                return console.log('player2 wins')
+                 console.log('player2 wins')
             };
-        } else if ((c2 === 0) || (c3 === 3)) {
+        } else if ((c2 === 0) || (c2 === 3)) {
             if(c2 === 0) {
-                return console.log('player1 wins');
+                 console.log('player1 wins');
             } else if (c2 === 3) {
-                return console.log('player2 wins')
+                 console.log('player2 wins')
             };
         } else if ((c3 === 0) || (c3 === 3)) {
             if(c3 === 0) {
-                return console.log('player1 wins');
+                console.log('player1 wins');
             } else if (c3 === 3) {
-                return console.log('player2 wins')
+                console.log('player2 wins')
             };
         } else if ((d1 === 0) || (d1 === 3)) {
             if(d1 === 0) {
-                return console.log('player1 wins');
+                console.log('player1 wins');
             } else if (d1 === 3) {
-                return console.log('player2 wins')
+                console.log('player2 wins')
             };
         } else if ((d2 === 0) || (d2 === 3)) {
             if(d2 === 0) {
-                return console.log('player1 wins');
+                 console.log('player1 wins');
             } else if (d2 === 3) {
-                return console.log('player2 wins')
+                console.log('player2 wins')
             };
-        } else {
-            return console.log('tie');
-            //return checkForTie();
-        };
-    }
+        } else if (gameBoard.turnCounter === 10) {
+            console.log('TIE');
+        } else {return};
+        } 
     return{checkWinConditions};
-}
+})();
 
 const boardDisplay = (function () {
     const boardGridDisplay = document.querySelectorAll('.gameboard > div');
     let i;
     let j;
     let writeToGrid = function () {
-            console.log(this);
             i = this.dataset.i;
-            console.log(this.dataset.i);
             j = this.dataset.j;
-            console.log(this.dataset.j);
             if(this.innerText === players.player1.playerMark) {
                 gameBoard.boardGrid[i][j] = 0;
             } else if (this.innerText === players.player2.playerMark) {
@@ -114,17 +103,19 @@ const boardDisplay = (function () {
         }
 
     function writeMarkValues () {
-        console.log(this);
+        //binds context of function to individual boardGridDisplay elements
         let bWriteToGrid = writeToGrid.bind(this); 
         if (gameBoard.turnCounter % 2 === 0) {
         //elements of an array  or  array-like structure are considered objects, so 'this' works
             this.innerText = players.player1.playerMark;
             bWriteToGrid();
+            gameFlow.checkWinConditions();
             this.removeEventListener('click', writeMarkValues);
             console.log(gameBoard.boardGrid);
         } else {
             this.innerText = players.player2.playerMark;
             bWriteToGrid();
+            gameFlow.checkWinConditions();
             this.removeEventListener('click', writeMarkValues);
             console.log(gameBoard.boardGrid);
         };
@@ -134,13 +125,7 @@ const boardDisplay = (function () {
         boardGridDisplay.forEach(element => {
             element.addEventListener('click', writeMarkValues);
         });
-        console.log(boardGridDisplay);
     }
     handlePlayerInput();
     
 })();
-
-
-
-const winCondition = controlGameFlow();
-winCondition.checkWinConditions();
